becd9905602670cc2c55cc485fa8564de75cfa08
JUHI1404 <juhimehta1404@gmail.com>
2024-08-02 14:57:45 +0530

    db setup

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require("express");
+require("dotenv").config();
+const router = require("./routes/router");
 
 const app = express();
 
-const port = 5000;
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use("/api", router);
+
+
 
 app.listen(port, () => {
-    console.log(`Server is running  on port ${port}`);
-  });
+  console.log(`Server is running  on port ${port}`);
+});
